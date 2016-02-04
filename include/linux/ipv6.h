0318c99af774cb17fbebfa8169024c2c9fb71c30
Johannes Berg <johannes.berg@intel.com>
2016-02-04 13:31:20 +0100

    ipv6: add option to drop unsolicited neighbor advertisements
    In certain 802.11 wireless deployments, there will be NA proxies
    that use knowledge of the network to correctly answer requests.
    To prevent unsolicitd advertisements on the shared medium from
    being a problem, on such deployments wireless needs to drop them.
    
    Enable this by providing an option called "drop_unsolicited_na".
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: danielhk <daniel_hk>

--- include/linux/ipv6.h
+++ include/linux/ipv6.h
@@ -53,6 +53,7 @@ struct ipv6_devconf {
 	__s32		accept_dad;
 	__s32		force_tllao;
 	__s32           ndisc_notify;
+	__s32		drop_unsolicited_na;
 	void		*sysctl;
 };
 
--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -4303,6 +4303,7 @@ static inline void ipv6_store_devconf(struct ipv6_devconf *cnf,
 	array[DEVCONF_RA_INFO_FLAG] = cnf->ra_info_flag;
 #endif	
 	array[DEVCONF_DROP_UNICAST_IN_L2_MULTICAST] = cnf->drop_unicast_in_l2_multicast;
+	array[DEVCONF_DROP_UNSOLICITED_NA] = cnf->drop_unsolicited_na;
 }
 
 static inline size_t inet6_ifla6_size(void)
@@ -5145,6 +5146,13 @@ static struct addrconf_sysctl_table
 		{
 			.procname	= "drop_unicast_in_l2_multicast",
 			.data		= &ipv6_devconf.drop_unicast_in_l2_multicast,
+			.maxlen		= sizeof(int),
+			.mode		= 0644,
+			.proc_handler	= proc_dointvec,
+		},
+		{
+			.procname	= "drop_unsolicited_na",
+			.data		= &ipv6_devconf.drop_unsolicited_na,
 			.maxlen		= sizeof(int),
 			.mode		= 0644,
 			.proc_handler	= proc_dointvec,
